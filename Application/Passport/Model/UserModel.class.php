43b17144a46dd0b89291a7cf10e097b7999b9ff4
maying <1977905246@qq.com>
2016-10-13 00:51:13 +0800

    user login

--- Application/Passport/Model/UserModel.class.php
+++ Application/Passport/Model/UserModel.class.php
@@ -2,5 +2,13 @@
 namespace Passport\Model;
 use Common\Model\BaseModel;
 class UserModel extends BaseModel {
+	public function getUserList($wehre,$offset,$limit){
+		$userList = $this->where($where)->limit($offset,$limit)->select();
+		return $userList;
+	}
 
+	public function getUserInfoByWhere($where) {
+		$user = $this->where($where)->find();
+		return $user;
+	}
 }

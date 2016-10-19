cb1d77990c4e8cda96174c936d390ff8874418da
maying <1977905246@qq.com>
2016-10-19 22:36:50 +0800

    跨域

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -4,6 +4,7 @@ function _ars($data,$flag,$is_die=true){
 	$result['status'] = $flag;
 	$result['result'] = $data;
 	header('Content-type: application/json');
+	header("Access-Control-Allow-Origin: *");
 	echo json_encode($result);
 	if ($is_die) {
 		die();

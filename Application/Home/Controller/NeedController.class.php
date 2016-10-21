1fac5bce8b5dc78333af45546f6802fdf7dbccad
zhaofei <1398805041@qq.com>
2016-10-21 22:30:04 +0800

    需求列表 搜索

--- Application/Home/Controller/NeedController.class.php
+++ Application/Home/Controller/NeedController.class.php
@@ -31,4 +31,37 @@ class NeedController extends CommonController{
 		$need->where(array('id'=>$id))->add($data);
 		$this->success('增加完成',U('Home/user/test'));
 	}
+	public function needList(){
+		$grade = M('grade')->where()->select();
+		$need = M('need')->where()->select();
+		$this->assign('grade',$grade);
+		$this->assign('need',$need);
+		$this->display();
+	}
+	public function search(){
+		$grade = $_POST['grade'];
+		$need = M('need')->where(array('grade_id'=>$grade))->select();
+		//var_dump($need);die();
+		$this->assign('need',$need);
+		$this->display();
+	}
+	public function needInfo(){
+		$id = $_GET['id'];
+        if(!$id){
+            echo $id;
+            $this->error("id error!!",U('home/need/needList'));
+        }
+    	$need_info = M('need')->where(array('id'=>$id))->find();
+        if(!is_array($need_info)||empty($need_info)){
+            $this->error("need error!!",U('home/need/needList'));
+        }
+    	$school = M('school')->where(array('id'=>$need_info['school_id']))->find();
+    	$grade = M('grade')->where(array('id'=>$need_info['grade_id']))->find();
+    	$subject = M('subject')->where(array('id'=>$need_info['subject_id']))->find();
+        $this->assign('need_info',$need_info);
+        $this->assign('school',$school);
+        $this->assign('grade',$grade);
+    	$this->assign('subject',$subject);
+    	$this->display();
+	}
 } 
